aa14d439607b1ff4ae7a5d36c04bb0ef914f5f7d
Porkodi177 <porkodicharulatha@gmai.com>
2026-03-27 17:36:39 +0530

    show blog posts

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function Dashboard() {
@@ -7,11 +7,27 @@ export default function Dashboard() {
 
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [posts, setPosts] = useState([]);
 
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
   };
+
+  const fetchPosts = async () => {
+    try {
+      const res = await axios.get(
+        "https://blog-platform-api-5n5q.onrender.com/api/posts"
+      );
+      setPosts(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = async (e) => {
     e.preventDefault();
@@ -27,9 +43,9 @@ export default function Dashboard() {
         }
       );
 
-      alert("Post created");
       setTitle("");
       setContent("");
+      fetchPosts();
     } catch (err) {
       alert("Error creating post");
     }
@@ -62,6 +78,16 @@ export default function Dashboard() {
 
         <button type="submit">Create</button>
       </form>
+
+      <h2>All Posts</h2>
+
+      {posts.map((post) => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <hr />
+        </div>
+      ))}
     </div>
   );
 }
