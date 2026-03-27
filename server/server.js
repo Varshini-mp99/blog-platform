553a0b6cca12b03015d694b2b211bda03305a150
Porkodi177 <porkodicharulatha@gmai.com>
2026-03-27 23:18:14 +0530

    fix create blog

--- server/server.js
+++ server/server.js
@@ -1,24 +1,48 @@
-require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
-const connectDB = require("./config/db");
-
-connectDB();
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API running...");
+mongoose.connect("YOUR_MONGODB_URL");
+
+const Post = mongoose.model("Post", {
+  title: String,
+  content: String,
 });
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/blogs", require("./routes/blogRoutes"));
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
 
-const PORT = process.env.PORT || 5000;
+/* GET POSTS */
+app.get("/posts", async (req, res) => {
+  const posts = await Post.find().sort({ _id: -1 });
+  res.json(posts);
+});
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+/* CREATE POST */
+app.post("/posts", async (req, res) => {
+  const { title, content } = req.body;
+
+  const post = new Post({
+    title,
+    content,
+  });
+
+  await post.save();
+  res.json(post);
+});
+
+/* DELETE POST */
+app.delete("/posts/:id", async (req, res) => {
+  await Post.findByIdAndDelete(req.params.id);
+  res.send("Deleted");
+});
+
+app.listen(10000, () => {
+  console.log("Server running on port 10000");
 });
