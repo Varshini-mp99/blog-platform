c4de0be1611d93bf5b8f013d92de94d695198dda
Porkodi177 <porkodicharulatha@gmai.com>
2026-03-27 23:42:16 +0530

    add login api

--- server/server.js
+++ server/server.js
@@ -7,59 +7,70 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* MongoDB connection */
 mongoose.connect(
 "mongodb+srv://admin:Varshini2004@cluster0.m6bt3di.mongodb.net/blogDB?retryWrites=true&w=majority"
 )
 .then(()=>console.log("MongoDB connected"))
 .catch(err=>console.log(err));
 
-/* Schema */
-const PostSchema = new mongoose.Schema({
-  title: String,
-  content: String
+/* User model */
+const User = mongoose.model("User",{
+email:String,
+password:String
 });
 
-const Post = mongoose.model("Post", PostSchema);
+/* Post model */
+const Post = mongoose.model("Post",{
+title:String,
+content:String
+});
 
 /* test */
-app.get("/", (req,res)=>{
-  res.send("API running");
+app.get("/",(req,res)=>{
+res.send("API running")
 });
 
-/* get posts */
-app.get("/posts", async (req,res)=>{
-  try{
-    const posts = await Post.find().sort({_id:-1});
-    res.json(posts);
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
+/* LOGIN */
+app.post("/login", async(req,res)=>{
+const {email,password} = req.body
 
-/* create */
-app.post("/posts", async (req,res)=>{
-  try{
-    const post = new Post(req.body);
-    await post.save();
-    res.json(post);
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
+const user = await User.findOne({email,password})
 
-/* delete */
-app.delete("/posts/:id", async (req,res)=>{
-  try{
-    await Post.findByIdAndDelete(req.params.id);
-    res.send("Deleted");
-  }catch(err){
-    res.status(500).json(err);
-  }
-});
+if(!user){
+return res.status(401).json("Invalid")
+}
 
-const PORT = process.env.PORT || 10000;
+res.json(user)
+})
 
-app.listen(PORT, ()=>{
-  console.log("Server running");
-});
+/* REGISTER (optional) */
+app.post("/register", async(req,res)=>{
+const user = new User(req.body)
+await user.save()
+res.json(user)
+})
+
+/* GET POSTS */
+app.get("/posts", async(req,res)=>{
+const posts = await Post.find().sort({_id:-1})
+res.json(posts)
+})
+
+/* CREATE POST */
+app.post("/posts", async(req,res)=>{
+const post = new Post(req.body)
+await post.save()
+res.json(post)
+})
+
+/* DELETE POST */
+app.delete("/posts/:id", async(req,res)=>{
+await Post.findByIdAndDelete(req.params.id)
+res.send("Deleted")
+})
+
+const PORT = process.env.PORT || 10000
+
+app.listen(PORT,()=>{
+console.log("Server running")
+})
